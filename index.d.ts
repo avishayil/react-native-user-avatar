3073048f67cc12d7241c171186a47e9f32caa293
杨俊宁 <youngjuning@aliyun.com>
2020-03-21 12:49:20 +0800

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ViewStyle, ImageStyle } from 'react-native'
+type UserAvatarPropsType = {
+  src?: string | number
+  name: string
+  color?: string
+  textColor?: string
+  colors?: string[]
+  size?: number
+  containerStyle?: ViewStyle
+  imageStyle?: ImageStyle
+  defaultName?: string
+  borderRadius?: number
+}
+
+export default class UserAvatar extends React.PureComponent<UserAvatarPropsType, any> {
+  static defaultProps: {}
+  constructor()
+  componentDidMount(): void
+  componentWillMount(): void
+  render(): JSX.Element
+}
