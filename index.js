e74d2ac8b2306baad22eedcf6aec33d70608ae13
Avishay Bar <avishay.il@gmail.com>
2020-04-23 12:20:01 +0300

    - continue refactor to components

--- index.js
+++ index.js
@@ -1,215 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import { View, Image, Text } from 'react-native';
-import initials from 'initials';
+import {View} from 'react-native';
+import {TextAvatar, ImageAvatar} from './components/';
+import {fetchImage, getContainerStyle, generateBackgroundStyle} from './helpers';
 
- function abbr(name) {
-  let abbr = initials(name);
-  if (name.startsWith('+')) {
-    abbr = `+${abbr}`;
-  }
-  if (!abbr) abbr = name;
-  return abbr;
-}
+const UserAvatar = (props) => {
+  const {
+    name,
+    src,
+    color,
+    colors,
+    textColor,
+    size,
+    imageStyle,
+    style,
+    borderRadius,
+    component,
+  } = props;
 
-function sumChars(str) {
-  let sum = 0;
-  for (let i = 0; i < str.length; i++) {
-    sum += str.charCodeAt(i);
-  }
+  // Validations
+  if (typeof (size) !== 'number') throw new Error('size prop should be a number');
 
-  return sum;
-}
+  const [inner, setInner] = useState(<TextAvatar textColor={textColor} size={size} name={name} />);
 
-export default class UserAvatar extends React.PureComponent {
-
-  static propTypes = {
-    name: PropTypes.string,
-    src: PropTypes.string,
-    color: PropTypes.string,
-    colors: PropTypes.array,
-    textColor: PropTypes.string,
-    size: PropTypes.number,
-    imageStyle: PropTypes.object,
-    style: PropTypes.object,
-    borderRadius: PropTypes.number,
-    component: PropTypes.any,
-  };
-
-  static defaultProps = {
-    size: 32,
-    textColor: '#fff',
-    name: 'John Doe',
-    colors: [ // from https://flatuicolors.com/
-      '#2ecc71', // emerald
-      '#3498db', // peter river
-      '#8e44ad', // wisteria
-      '#e67e22', // carrot
-      '#e74c3c', // alizarin
-      '#1abc9c', // turquoise
-      '#2c3e50', // midnight blue
-    ],
-  }
-
-  state = {
-    inner: this.renderTextElement(),
-  };
-
-  componentDidMount() {
-    this.initialize();
-  }
-
-  initialize() {
-    const {
-      src,
-      component,
-    } = this.props;
-    if (component) {
-      this.setState({
-        inner: component,
-      });
-    }
+  useEffect(() => {
+    if (component) setInner(component);
     if (src) {
-      this.isSrcAvailable().then((isImage) => {
-        if (isImage) {
-          this.setState({
-            inner: this.renderImageSrcElement(),
-          });
-        } else {
-          this.setState({
-            inner: this.renderTextElement(),
-          });
-        }
-      }).catch(() => {
-        this.setState({
-          inner: this.renderTextElement(),
-        });
+      fetchImage(src).then((isImage) => {
+        if (isImage) setInner(<ImageAvatar src={src} size={size} imageStyle={imageStyle} />);
       });
     }
-  }
+  }, []);
 
-  async isSrcAvailable() {
-    const {
-      src,
-    } = this.props;
+  return (
+    <View style={[generateBackgroundStyle(name, color, colors), getContainerStyle(size, src, borderRadius), style]}>
+      {inner}
+    </View>
+  );
+};
 
-    try {
-      const fetchCall = await fetch(src);
-      if (fetchCall._bodyBlob._data.type.startsWith('image/')) {
-        return true;
-      }
-    } catch (err) {
-      console.warn('Error fetching the online image source, falling back to initials');
-      return false;
-    }
-  }
+UserAvatar.propTypes = {
+  name: PropTypes.string,
+  src: PropTypes.string,
+  color: PropTypes.string,
+  colors: PropTypes.array,
+  textColor: PropTypes.string,
+  size: PropTypes.number,
+  imageStyle: PropTypes.object,
+  style: PropTypes.object,
+  borderRadius: PropTypes.number,
+  component: PropTypes.any,
+};
 
-  renderTextElement() {
-    const {
-      textColor,
-      size,
-      name
-    } = this.props;
+UserAvatar.defaultProps = {
+  size: 32,
+  textColor: '#fff',
+  name: 'John Doe',
+  colors: [ // from https://flatuicolors.com/
+    '#2ecc71', // emerald
+    '#3498db', // peter river
+    '#8e44ad', // wisteria
+    '#e67e22', // carrot
+    '#e74c3c', // alizarin
+    '#1abc9c', // turquoise
+    '#2c3e50', // midnight blue
+  ],
+};
 
-    return (
-      <View style={[this.getTextContainerStyle()]}>
-        <Text
-          style={{
-            color: textColor,
-            fontSize: size / 2.5,
-          }}
-          adjustsFontSizeToFit={true}
-        >
-          {abbr(name)}
-        </Text>
-      </View>
-    );
-  }
-
-  renderImageSrcElement() {
-    const {
-      src,
-      size,
-      imageStyle,
-      borderRadius = size * 0.5,
-    } = this.props;
-
-    const imageDefaultStyle = {
-      borderRadius,
-      width: size,
-      height: size,
-    };
-
-    const props = {
-      style: [imageDefaultStyle, imageStyle],
-      source: { uri: src },
-    };
-
-    return React.createElement(Image, props);
-  }
-
-  getBackgrond() {
-    const {
-      name,
-      color,
-      colors,
-    } = this.props;
-
-    let background;
-    if (color) {
-      background = color;
-    } else {
-      // Pick a deterministic color from the list
-      const i = sumChars(name) % colors.length;
-      background = colors[i];
-    }
-    return { backgroundColor: background };
-  }
-
-  getContainerStyle() {
-    const {
-      size,
-      src,
-      borderRadius = size * 0.5,
-    } = this.props;
-
-    return {
-      borderRadius,
-      borderWidth: src ? 0 : 1,
-      borderColor: 'transparent',
-      justifyContent: 'center',
-      alignItems: 'center',
-    };
-  };
-
-  getTextContainerStyle() {
-    const {
-      size,
-    } = this.props;
-
-    return {
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: -(size / 20),
-      height: size,
-      width: size,
-    };
-  };
-
-  render() {
-    const {
-      size,
-      style,
-    } = this.props;
-
-    // Validations
-    if (typeof (size) !== 'number') throw new Error('size prop should be a number');
-
-    return (
-      <View style={[this.getContainerStyle(), this.getBackgrond(), style]}>
-        {this.state.inner}
-      </View>
-    );
-  }
-}
+export default UserAvatar;
