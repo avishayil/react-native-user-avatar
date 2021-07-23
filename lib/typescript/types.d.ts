0faedc29cbc68e52f8380d5e0fc2f6a1d44d06d0
jefferscn <16450382@qq.com>
2021-07-23 17:51:27 +0800

    add lib directory for direct install package from github

---
+++ lib/typescript/types.d.ts
@@ -0,0 +1,14 @@
+import { ImageStyle, StyleProp, ViewStyle, TextStyle } from 'react-native';
+export declare type UserAvatarPropsType = {
+    name: string;
+    src: string;
+    bgColor: string;
+    bgColors: string[];
+    textColor: string;
+    size: number;
+    imageStyle: ImageStyle;
+    style?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
+    borderRadius: number;
+    component: Object;
+};
